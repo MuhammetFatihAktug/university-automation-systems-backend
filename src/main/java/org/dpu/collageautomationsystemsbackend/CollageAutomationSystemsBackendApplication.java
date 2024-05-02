5db960010ab15be5ea0f0f683dd62061913da26c
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-05-02 20:45:49 +0300

    Initial commit

---
+++ src/main/java/org/dpu/collageautomationsystemsbackend/CollageAutomationSystemsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dpu.collageautomationsystemsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollageAutomationSystemsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollageAutomationSystemsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dpu/collageautomationsystemsbackend/CollageAutomationSystemsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dpu.collageautomationsystemsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollageAutomationSystemsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
