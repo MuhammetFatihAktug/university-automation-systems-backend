52300b183e673199fc601020c6226416be7e98bd
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-05-04 15:36:37 +0300

    security config added and checked

--- src/main/java/org/dpu/collageautomationsystemsbackend/CollageAutomationSystemsBackendApplication.java
+++ src/main/java/org/dpu/collageautomationsystemsbackend/CollageAutomationSystemsBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CollageAutomationSystemsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CollageAutomationSystemsBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CollageAutomationSystemsBackendApplication.class, args);
+    }
 
 }
---
+++ src/main/java/org/dpu/collageautomationsystemsbackend/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.dpu.collageautomationsystemsbackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(HttpMethod.POST, "/messages").permitAll()
+                        .anyRequest().permitAll())
+        ;
+        return http.build();
+    }
+
+}
